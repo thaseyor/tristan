38a6999e26e1401831a133840abde8e2ade24b44
thaseyor <mikeston@mail.ru>
2022-08-11 16:39:41 +0600

    refactor: cleaner code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod config;
+mod service;
 
-use config::Command;
-use teloxide::{prelude::*, utils::command::BotCommands};
+use teloxide::prelude::*;
 extern crate pretty_env_logger;
 
 #[macro_use]
@@ -15,76 +15,54 @@ async fn main() {
     let token = config::get_token();
     let bot = Bot::new(token).auto_send();
 
-    let res = bot
-        .set_my_default_administrator_rights()
-        .rights(config::BOT_RIGHTS)
-        .send()
-        .await;
+    let res = service::set_default_rights(&bot).await;
 
     if let Err(e) = res {
         error!("{:?}", e);
     }
 
     teloxide::repl(bot, |m: Message, bot: AutoSend<Bot>| async move {
-        if m.chat.is_private() {
+        let is_private_chat = m.chat.is_private();
+
+        if is_private_chat {
             let text = m.text();
 
-            match text {
-                None => {}
-                Some(text) => {
-                    let me = bot.get_me().await?;
+            if let Some(text) = text {
+                let chat_id = m.chat.id;
+                let res = service::send_response(bot, chat_id, text).await;
 
-                    let res = Command::parse(
-                        text,
-                        me.user
-                            .username
-                            .as_deref()
-                            .expect("Bots must have usernames"),
-                    );
-
-                    let response = match res {
-                        Ok(command) => get_command_response(command),
-                        Err(_e) => config::BOT_DESCRIPTION.to_string(),
-                    };
-
-                    bot.send_message(m.chat.id, response).await?;
+                if let Err(e) = res {
+                    error!("{:?}", e);
                 }
-            }
+            };
         } else {
             let leaved = m.left_chat_member();
             let joined = m.new_chat_members();
 
-            if joined != None || leaved != None {
-                let admins = bot.get_chat_administrators(m.chat.id).send().await?;
-
-                let my_id = bot.get_me().await?.id;
-                let owner_id = admins.iter().find(|a| a.kind.is_owner()).unwrap().user.id;
-                let chat_title = m.chat.title().unwrap_or("your group");
-
-                let me_as_admin = admins.iter().find(|a| a.user.id == my_id);
-
-                match me_as_admin {
-                    Some(me_as_admin) => {
-                        if me_as_admin.can_delete_messages() {
-                            bot.delete_message(m.chat.id, m.id).await?;
-                        } else {
-                            bot.send_message(
-                                owner_id,
-                                format!(
-                                    "Give me permission to delete messages pls in {}",
-                                    chat_title
-                                ),
-                            )
-                            .await?;
+            // check if smbd joined or leaved
+            if joined.is_some() || leaved.is_some() {
+                // check if bot is the one who left the group
+                if let Some(leaved) = leaved {
+                    let me = bot.get_me().await;
+                    match me {
+                        Ok(me) => {
+                            if leaved.id == me.id {
+                                return respond(());
+                            }
                         }
+                        Err(e) => error!("{:?}", e),
                     }
-                    None => {
-                        bot.send_message(
-                            owner_id,
-                            format!("Promote me to admin pls in {}", chat_title),
-                        )
-                        .await?;
-                    }
+                }
+
+                let chat_id = m.chat.id;
+                let chat_title = m.chat.title().unwrap_or_else(|| "your group");
+                let message_id = m.id;
+
+                let res =
+                    service::delete_system_message(bot, chat_id, chat_title, message_id).await;
+
+                if let Err(e) = res {
+                    error!("{:?}", e);
                 }
             }
         }
@@ -92,11 +70,4 @@ async fn main() {
         respond(())
     })
     .await;
-}
-
-fn get_command_response(command: Command) -> String {
-    match command {
-        Command::Start => config::BOT_DESCRIPTION.to_string(),
-        Command::Help => Command::descriptions().to_string(),
-    }
 }
---
+++ src/service.rs
@@ -0,0 +1,86 @@
+use crate::config::{self, Command};
+use teloxide::{prelude::*, utils::command::BotCommands, RequestError};
+
+pub async fn set_default_rights(bot: &AutoSend<Bot>) -> Result<(), RequestError> {
+    bot.set_my_default_administrator_rights()
+        .rights(config::BOT_RIGHTS)
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+pub async fn delete_system_message(
+    bot: AutoSend<Bot>,
+    chat_id: ChatId,
+    chat_title: &str,
+    message_id: i32,
+) -> Result<(), RequestError> {
+    let admins = bot.get_chat_administrators(chat_id).send().await?;
+
+    if admins.len() == 0 {
+        return Ok(());
+    }
+
+    let my_id = bot.get_me().await?.id;
+    let owner_id = admins
+        .iter()
+        .find(|a| a.is_owner())
+        .expect("Any group must have an owner")
+        .user
+        .id;
+
+    let me_as_admin = admins.iter().find(|a| a.user.id == my_id);
+
+    match me_as_admin {
+        Some(me_as_admin) if me_as_admin.can_delete_messages() => {
+            bot.delete_message(chat_id, message_id).await?;
+        }
+        Some(_) => {
+            bot.send_message(
+                owner_id,
+                format!(
+                    "Give me permission to delete messages pls in {}",
+                    chat_title
+                ),
+            )
+            .await?;
+        }
+        None => {
+            bot.send_message(
+                owner_id,
+                format!("Promote me to admin pls in {}", chat_title),
+            )
+            .await?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn send_response(
+    bot: AutoSend<Bot>,
+    chat_id: ChatId,
+    text: &str,
+) -> Result<(), RequestError> {
+    let me = bot.get_me().await?;
+    let username = me.user.username.expect("Bots must have usernames");
+
+    let response = get_command_response(&text, &username);
+    bot.send_message(chat_id, response).await?;
+
+    Ok(())
+}
+
+fn get_command_response(text: &str, username: &str) -> String {
+    let res = Command::parse(text, username);
+    let response = match res {
+        Ok(command) => match command {
+            Command::Start => config::BOT_DESCRIPTION.to_string(),
+            Command::Help => Command::descriptions().to_string(),
+        },
+        Err(_e) => config::BOT_DESCRIPTION.to_string(),
+    };
+
+    return response;
+}
